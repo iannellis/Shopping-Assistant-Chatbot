8088abe109f774b87fb4da31c56435502c4db235
Ian Ellis <iellis@minevaluation.com>
2024-12-07 12:44:41 -0700

    Updated for re-run on Computer 3

--- code/Llama_data_checks/check_image_category.py
+++ code/Llama_data_checks/check_image_category.py
@@ -20,8 +20,10 @@ model = MllamaForConditionalGeneration.from_pretrained(
 )
 processor = AutoProcessor.from_pretrained(model_id)
 
+shoptalk_blobs_dir = "../ShopTalk-blobs/"
+
 def load_listings():
-    pdf = pd.read_pickle("../Capstone Project - ShopTalk/ABO_dataset/abo-listings-english-tags.pkl")
+    pdf = pd.read_pickle(shoptalk_blobs_dir + "ABO_dataset/abo-listings-english-tags.pkl")
     pdf['product_type'] = pdf['product_type'].str.replace('_', ' ')
     pdf.loc[pdf['product_type'] == 'FINERING', 'product_type'] = 'FINE RING'
     pdf.loc[pdf['product_type'] == 'FINENECKLACEBRACELETANKLET', 'product_type'] = 'FINE NECKLACE BRACELET ANKLET'
@@ -60,7 +62,7 @@ def run_dataset_check(pdf_has_images, iloc_start, iloc_end, image_path_prefix, s
 def resume_dataset_check(pdf_has_images, iloc_start, iloc_end, image_path_prefix, save_name):
     image_meta_df = load_image_meta()    
 
-    image_categroy_match_df = pd.read_pickle('../Capstone Project - ShopTalk/ABO_dataset/abo-category-check-comp-2.pkl')
+    image_categroy_match_df = pd.read_pickle(shoptalk_blobs_dir + 'ABO_dataset/' + save_name)
     
     image_categroy_match = image_categroy_match_df.to_dict(orient='list')
     image_categroy_match['image_id'] = image_categroy_match_df.to_dict(orient='split')['index']
@@ -90,7 +92,7 @@ def resume_dataset_check(pdf_has_images, iloc_start, iloc_end, image_path_prefix
     save_dataset(image_categroy_match, save_name)
 
 def load_image_meta():
-    image_meta_path = '../Capstone Project - ShopTalk/ABO_dataset/images/metadata/images.csv'
+    image_meta_path = shoptalk_blobs_dir + 'ABO_dataset/images/metadata/images.csv'
     image_meta_df = pd.read_csv(image_meta_path).set_index('image_id')
     return image_meta_df
 
@@ -123,4 +125,4 @@ def llama_check_image_category(product_type, image):
 
 def save_dataset(image_categroy_match, save_name):
     image_categroy_match_df = pd.DataFrame(image_categroy_match).set_index('image_id')
-    image_categroy_match_df.to_pickle("../Capstone Project - ShopTalk/ABO_dataset/" + save_name)
+    image_categroy_match_df.to_pickle(shoptalk_blobs_dir + "ABO_dataset/" + save_name)
