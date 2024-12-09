c3ef59ad688ffc71e983ba81cf69f770db17006a
Ian Ellis <iellis@minevaluation.com>
2024-12-09 11:36:57 -0700

    Missed some items, so had to resume again

--- code/Llama_data_checks/computer_3_run.py
+++ code/Llama_data_checks/computer_3_run.py
@@ -4,5 +4,5 @@ pdf = load_listings()
 pdf_has_images = pdf[~pdf['main_image_id'].isna() | ~pdf['other_image_id'].isna()]
 n_products = len(pdf_has_images)
 
-run_dataset_check(pdf_has_images, n_products//2, n_products//2 + 45000, 'D:/images/small', 'abo-category-check-comp-3-run2.pkl')
-# resume_dataset_check(pdf_has_images, n_products//2, n_products//2 + 45000, 'D:/images/small', 'abo-category-check-comp-3-run2.pkl')
+# run_dataset_check(pdf_has_images, n_products//2, n_products//2 + 45000, 'D:/images/small', 'abo-category-check-comp-3-run2.pkl')
+resume_dataset_check(pdf_has_images, n_products//2, n_products//2 + 45054, 'D:/images/small', 'abo-category-check-comp-3-run2.pkl')
