3d36690d1dc88f07084e57ec334d3b27cbeb9368
Ian Ellis <iellis@minevaluation.com>
2024-12-09 16:21:38 -0700

    There were no missing items

--- code/Llama_data_checks/computer_3_run.py
+++ code/Llama_data_checks/computer_3_run.py
@@ -1,15 +1,8 @@
-from check_image_category import (load_listings, run_dataset_check, resume_dataset_check,
-                                  run_missing_items)
-import pickle
+from check_image_category import load_listings, run_dataset_check, resume_dataset_checks
 
 pdf = load_listings()
 pdf_has_images = pdf[~pdf['main_image_id'].isna() | ~pdf['other_image_id'].isna()]
 n_products = len(pdf_has_images)
 
-# run_dataset_check(pdf_has_images, n_products//2, n_products//2 + 45000, 'D:/images/small', 'abo-category-check-comp-3-run2.pkl')
+run_dataset_check(pdf_has_images, n_products//2, n_products//2 + 45000, 'D:/images/small', 'abo-category-check-comp-3-run2.pkl')
 # resume_dataset_check(pdf_has_images, n_products//2, n_products//2 + 45054, 'D:/images/small', 'abo-category-check-comp-3-run2.pkl')
-
-with open('code/Llama_data_checks/missing_items.pkl', 'rb') as f:
-    missing_items = pickle.load(f)
-        
-run_missing_items(pdf_has_images, missing_items, 'D:/images/small', 'abo-category-check-missing-items.pkl')
