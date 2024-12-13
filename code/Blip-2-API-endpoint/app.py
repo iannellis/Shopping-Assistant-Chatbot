2dff0776745f2ad2196aae0cf6683d8fc1b5b6c8
Ian Ellis <iellis@minevaluation.com>
2024-12-13 15:44:23 -0700

    Fix some bugs found during endpoint testing.

--- code/Blip-2-API-endpoint/app.py
+++ code/Blip-2-API-endpoint/app.py
@@ -15,12 +15,14 @@ app = FastAPI(title='BLIP-2 embeddings', openapi_url="/openapi.json")
 api_router = APIRouter()
 
 # the (eval) preprocessors for the model
-with open('D:/pickles/processors.pkl', 'rb') as f:
+with open('/mnt/d/pickles/processors.pkl', 'rb') as f:
     vis_processor, text_processor = pickle.load(f)
 
 # the model itself
 print('Loading model')
-model = torch.load("D:/marqo-gs-10m/model-saves/pretrain_1epoch.pt")
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = torch.load("/mnt/d/marqo-gs-10m/model-saves/pretrain_1epoch.pt")
+model.to(device)
 model.eval()
 print('Done loading model')
 
@@ -47,16 +49,16 @@ def root() -> dict:
     return {"msg": "You can use this endpoint to get BLIP-2 embeddings."}
 
 @api_router.post("/embed_image/", status_code=201)
-async def create_recipe(*, input: ImageInput) -> dict:
+async def embed_image(*, input: ImageInput) -> dict:
     """
     Get image embeddings
     """
     image = decode_image(input.image)
-    image_tensor = vis_processor(image)
+    image_tensor = vis_processor(image).unsqueeze(0).to(device)
     
     # Generate embedding
     sample = {'image': image_tensor}
-    embedding = model.extract_features(sample, mode='image').image_embeds[0,0,:].detach().cpu().to_list()
+    embedding = model.extract_features(sample, mode='image').image_embeds[0,0,:].detach().cpu().tolist()
     return {"embedding": embedding}
 
 @api_router.post("/embed_text", status_code=201)
@@ -68,7 +70,7 @@ async def embed_text(input: TextInput):
     
     # Generate embedding
     sample = {'text_input': [text]}
-    embedding = model.extract_features(sample, mode='text').text_embeds[0,0,:].detach().cpu().to_list()
+    embedding = model.extract_features(sample, mode='text').text_embeds[0,0,:].detach().cpu().tolist()
     return {"embedding": embedding}
 
 @api_router.post("/embed_multimodal", status_code=201)
@@ -77,12 +79,12 @@ async def embed_multimodal(input: MultimodalInput):
     Get multimodal embeddings
     """
     image = decode_image(input.image)
-    image_tensor = vis_processor(image)
+    image_tensor = vis_processor(image).unsqueeze(0).to(device)
     text = text_processor(input.text)
     
     # Generate embedding
     sample = {'image': image_tensor, 'text_input': [text]}
-    embedding = model.extract_features(sample, mode='multimodal').multimodal_embeds[0,0,:].detach().cpu().to_list()
+    embedding = model.extract_features(sample, mode='multimodal').multimodal_embeds[0,0,:].detach().cpu().tolist()
     return {"embedding": embedding}
 
 app.include_router(api_router, prefix="/api/v1")
@@ -92,4 +94,4 @@ if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=9001, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=9000, log_level="debug")
