1a201980238556d8c46e56f64cd6d02b7939985c
Ian Ellis <iellis@minevaluation.com>
2025-01-08 16:41:13 -0700

    Add docstrings

--- code/Blip-2_embeddings/embeddings.py
+++ code/Blip-2_embeddings/embeddings.py
@@ -12,8 +12,20 @@ from tqdm import tqdm
 model_paths = {'gs': '/mnt/d/marqo-gs-10m/model-saves/pretrain_1epoch.pt',
                'abo': '/mnt/d/abo-dataset/model_saves/pretrain_2epochs.pt'}
 
-def run_embeddings(abo_dataset_dir='/mnt/d/abo-dataset', model_type='pretrain', 
-                   device='cuda', save_path='/mnt/d/embeddings', batch_size=64):
+def run_embeddings(abo_dataset_dir='/mnt/d/abo-dataset/', model_type='pretrain', 
+                   device='cuda', save_path='/mnt/d/embeddings/', batch_size=64):
+    """Run the data throught the multimodal (text + image) and text-only feature
+    extractor of the BLIP-2 mmodel to create embeddings.
+
+    Args:
+        abo_dataset_dir (str, optional): The directory where the ABO dataset has been extracted to
+        model_type (str, optional): Which version of the BLIP-2 model to use. Options are
+                                    'coco,' 'pretrain,' 'gs,' and 'abo.' See documentation for
+                                    details.
+        device (str, optional): Device to load the model to. Usually 'cpu' or 'cuda'.
+        save_path (str, optional): The directory to dumpt the embeddings into.
+        batch_size (int, optional): The batch size for calculating the embeddings.
+    """  
     images_dir = abo_dataset_dir + '/images/small'
     metadata_file = abo_dataset_dir + '/abo-listings-final-draft.pkl'
     image_metadata_file = abo_dataset_dir + '/images/metadata/images.csv'
@@ -40,6 +52,8 @@ def run_embeddings(abo_dataset_dir='/mnt/d/abo-dataset', model_type='pretrain',
 def build_abo_dataloader_multimodal(images_dir: str, metadata_file: str, image_metadata_file: str, 
                          image_processor: callable, text_processor: callable, 
                          batch_size=64, num_workers=2) -> DataLoader:
+    """Load the data to build the multimodal (image + text) dataloader for the ABO dataset
+    and build it."""
     metadata = pd.read_pickle(metadata_file)
     image_metadata = pd.read_csv(image_metadata_file).set_index('image_id')
     image_item_pairs = abo_image_item_pairs(metadata)
@@ -52,6 +66,8 @@ def build_abo_dataloader_multimodal(images_dir: str, metadata_file: str, image_m
     return dataloader_multimodal
 
 def abo_image_item_pairs(metadata: pd.DataFrame) -> pd.DataFrame:
+    """Takes the ABO metadata extracts all the image_ids associated with each item_id,
+    and produces image-item (image_id-item_id) pairs."""
     image_ids = []
     item_ids = []
     for item_id in metadata.index:
@@ -70,7 +86,11 @@ def abo_image_item_pairs(metadata: pd.DataFrame) -> pd.DataFrame:
     return pd.DataFrame({'image_id': image_ids, 'item_id': item_ids})
 
 class ABODataset_multimodal(Dataset):
-    # Note: modified from fine-tuning version
+    """The dataloader for ABO multimodal data. Loads the multimodal data (image-item pairs)
+    for creating the embedding. Takes an index in the image_item_pairs dataframe, then
+    uses the corresponding image_id and item_id to load the image and produce a text string
+    from the row of metadata.
+    Note: modified from fine-tuning version"""
     def __init__(self, image_dir: str, metadata: pd.DataFrame,
                  image_metadata: pd.DataFrame, image_item_pairs: pd.DataFrame,
                  image_processor: callable, text_processor: callable):
@@ -84,6 +104,8 @@ class ABODataset_multimodal(Dataset):
         self._reorg_metadata_columns()
         
     def _reorg_metadata_columns(self):
+        """Put the metadata dataframe columns in an order we want them in for producing
+        the text strings."""
         # drop item_keywords because there can be rediculously many
         self.metadata = self.metadata[['item_name', 'brand', 'model_name', 'model_year',
                                        'product_description', 'product_type', 'color',
@@ -105,6 +127,7 @@ class ABODataset_multimodal(Dataset):
         return image, label, image_id, item_id
     
     def _row_to_str(self, row):
+        """Convert a row of metadata to a string the model can consume."""
         row_filtered = row.dropna()
         text = []
         for row_item in row_filtered:
@@ -118,7 +141,7 @@ class ABODataset_multimodal(Dataset):
     
 def build_abo_dataloader_text(metadata_file: str, text_processor: callable,
                               batch_size=64, num_workers=2) -> DataLoader:
-    
+    """Load the data to build the text-only dataloader for the ABO dataset and build it."""
     metadata = pd.read_pickle(metadata_file)
     dataset_text = ABODataset_text(metadata, text_processor)
     dataloader_text = DataLoader(dataset=dataset_text, batch_size=batch_size,
@@ -126,6 +149,9 @@ def build_abo_dataloader_text(metadata_file: str, text_processor: callable,
     return dataloader_text
 
 class ABODataset_text(ABODataset_multimodal):
+    """The dataloader for ABO text-only data. Loads the text data for creating the
+    embedding. Takes an integer index in the metadata dataframe then turns the row into
+    a text string for creating embeddings."""
     def __init__(self, metadata: pd.DataFrame, text_processor: callable):
         self.metadata = metadata
         self.text_processor = text_processor
@@ -142,6 +168,8 @@ class ABODataset_text(ABODataset_multimodal):
         return label, item_id
         
 def embed_multimodal(model, dataloader, device, save_path, model_type):
+    """Enumerate through the multimodal dataloader and create the embeddings. Create a
+    Pandas dataframe from them and save to a pickle file."""
     embeddings_dict = {'image_id': [], 'item_id': [], 'embedding': []}
     
     model.eval()
@@ -166,6 +194,8 @@ def embed_multimodal(model, dataloader, device, save_path, model_type):
     return
 
 def embed_text(model, dataloader, save_path, model_type):
+    """Enumerate through the text dataloader and create the embeddings. Create a
+    Pandas dataframe from them and save to a pickle file."""
     embeddings_dict = {'item_id': [], 'embedding': []}
     
     model.eval()
