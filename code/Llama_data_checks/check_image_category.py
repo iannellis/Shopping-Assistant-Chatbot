1424b1287c57ebac1cc51a033730f96dfcf57d74
Ian Ellis <iellis@minevaluation.com>
2025-01-08 14:33:59 -0700

    Add docstrings

--- code/Llama_data_checks/check_image_category.py
+++ code/Llama_data_checks/check_image_category.py
@@ -1,4 +1,10 @@
-# Uses the 3.12 environment locally
+"""Run images and corresponding product_types through Llama-vision to check if the items
+are categorized correctly. Remove the product_type if not.
+
+Note that this file has a restart function to restart a run that crashed.
+
+Uses the 3.12 environment locally
+"""
 
 import torch
 from transformers import MllamaForConditionalGeneration, AutoProcessor, BitsAndBytesConfig
@@ -17,7 +23,7 @@ model = MllamaForConditionalGeneration.from_pretrained(
     torch_dtype=torch.bfloat16,
     device_map="auto",
     do_sample=True,
-    temperature=0.01,
+    temperature=0.01, # model can get inconsistent if we use defaults for last 2 options
     top_p=0
 )
 processor = AutoProcessor.from_pretrained(model_id)
@@ -25,6 +31,7 @@ processor = AutoProcessor.from_pretrained(model_id)
 shoptalk_blobs_dir = "../ShopTalk-blobs/"
 
 def load_listings():
+    """Load the metadata and fix the product_type so that the words are separated by sapces."""
     pdf = pd.read_pickle(shoptalk_blobs_dir + "ABO_dataset/abo-listings-english-tags.pkl")
     pdf['product_type'] = pdf['product_type'].str.replace('_', ' ')
     pdf.loc[pdf['product_type'] == 'FINERING', 'product_type'] = 'FINE RING'
@@ -39,6 +46,15 @@ def load_listings():
     return pdf
 
 def run_dataset_check(pdf_has_images, iloc_start, iloc_end, image_path_prefix, save_name):
+    """Run the dataset through the model, checking if each image matches the product_type.
+
+    Args:
+        pdf_has_images: the dataframe with the metadata just for products with images
+        iloc_start (int): the integer index in pdf_has_images to start the run at
+        iloc_end (int): the integer index in pdf_has_images to end the run at
+        image_path_prefix (str): the path to the directory with the images in it
+        save_name (str): the file name for saving the results of the run
+    """
     image_meta_df = load_image_meta()    
 
     image_categroy_match = {'image_id': [], 'item_id': [], 'product_type': [], 'match': []}
@@ -62,6 +78,16 @@ def run_dataset_check(pdf_has_images, iloc_start, iloc_end, image_path_prefix, s
     save_dataset(image_categroy_match, save_name)
 
 def resume_dataset_check(pdf_has_images, iloc_start, iloc_end, image_path_prefix, save_name):
+    """Resume the run of the dataset through the model, checking if each image matches 
+    the product_type.
+
+    Args:
+        pdf_has_images: the dataframe with the metadata just for products with images
+        iloc_start (int): the integer index in pdf_has_images to start the run at
+        iloc_end (int): the integer index in pdf_has_images to end the run at
+        image_path_prefix (str): the path to the directory with the images in it
+        save_name (str): the file name for saving the results of the run
+    """
     image_meta_df = load_image_meta()    
 
     image_categroy_match_df = pd.read_pickle(shoptalk_blobs_dir + 'ABO_dataset/' + save_name)
@@ -94,11 +120,13 @@ def resume_dataset_check(pdf_has_images, iloc_start, iloc_end, image_path_prefix
     save_dataset(image_categroy_match, save_name)
 
 def load_image_meta():
+    """Load the image metadata. Tells us where each image is given its image_id."""
     image_meta_path = shoptalk_blobs_dir + 'ABO_dataset/images/metadata/images.csv'
     image_meta_df = pd.read_csv(image_meta_path).set_index('image_id')
     return image_meta_df
 
 def get_all_image_ids(row):
+    """Given a metadata row, get all the images_ids from it."""
     image_ids = []
     if isinstance(row['main_image_id'], str):
         image_ids.append(row['main_image_id'])
@@ -108,6 +136,8 @@ def get_all_image_ids(row):
     return image_ids
 
 def llama_check_image_category(product_type, image):
+    """Given a product_type and a PIL image, run both through the model to check whether
+    they match."""
     messages = [
         {"role": "user", "content": [
             {"type": "image"},
@@ -125,6 +155,7 @@ def llama_check_image_category(product_type, image):
     output = model.generate(**inputs, max_length=100)
     return processor.decode(output[0])[-14:-11].strip()
 
-def save_dataset(image_categroy_match, save_name):
-    image_categroy_match_df = pd.DataFrame(image_categroy_match).set_index('image_id')
+def save_dataset(image_category_match, save_name):
+    """Save the current progress."""
+    image_categroy_match_df = pd.DataFrame(image_category_match).set_index('image_id')
     image_categroy_match_df.to_pickle(shoptalk_blobs_dir + "ABO_dataset/" + save_name)
