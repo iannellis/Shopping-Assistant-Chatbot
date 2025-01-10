4278e58b3046cf5ff6760d84fce304a5fc40afa3
Ian Ellis <iellis@minevaluation.com>
2025-01-09 18:08:55 -0700

    update for Python file name changes

--- code/Data_pipeline/run_pipeline.sh
+++ code/Data_pipeline/run_pipeline.sh
@@ -3,8 +3,8 @@
 python311=/home/ellis/.cache/pypoetry/virtualenvs/shoptalk-py3-11-gySnKGNK-py3.11/bin/python
 python312=/home/ellis/.cache/pypoetry/virtualenvs/shoptalk-py3-12-PgppYsjg-py3.12/bin/python
 
-$python312 1_english_tags.py
-$python312 2_product_type_spaces.py
-$python312 3_english_check.py
-$python312 4_product_type_verification.py
-$python311 5_embed_to_chroma.py
+$python312 english_tags_1.py
+$python312 product_type_spaces_2.py
+$python312 english_check_3.py
+$python312 product_type_verification_4.py
+$python311 embed_to_chroma_5.py
